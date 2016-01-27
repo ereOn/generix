5c6f4e20d84f865156ba93c82dcd0d874ff46d64
Julien Kauffmann <julien.kauffmann@freelan.org>
2016-01-26 23:33:23 -0500

    Added real integration tests based on examples

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -1,6 +1,7 @@
 import os
 
 from functools import partial
+from filecmp import dircmp
 
 from click.testing import CliRunner
 from pytest import (
@@ -29,6 +30,13 @@ def runner(chwd):
     return run
 
 
+def compare_folders(left, right):
+    cmp = dircmp(str(left), str(right))
+    assert cmp.left_only == []
+    assert cmp.right_only == []
+    assert cmp.diff_files == []
+
+
 @mark.parametrize('folder,index_file,data_file', [
     ('examples/fruits', 'index.yml', 'data.yml'),
     ('examples/vehicles', 'index.yml', 'data.json'),
@@ -37,3 +45,5 @@ def runner(chwd):
 def test_examples(tmpdir, runner, index_file, data_file):
     result = runner(index_file, data_file, '-o', tmpdir.join('output'))
     assert result.exit_code == 0
+
+    compare_folders('output', tmpdir.join('output'))
